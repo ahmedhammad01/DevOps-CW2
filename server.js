6b4efda53aa40d3ce725fdc67c1058bb48fdb362
ahmedhammad01 <ma6922ma@gmail.com>
2025-04-16 00:49:09 +0000

    Add initial server.js for DevOps Coursework 2

--- server.js
+++ server.js
@@ -9,11 +9,13 @@ var handleRequest = function(request, response) {
   response.writeHead(200);
   response.write("DevOps Coursework 2! | Running on: ");
   response.write(host);
-  response.end(" | v=1\n");
-  console.log("Running On:" ,host, "| Total Requests:", ++requests,"| App Uptime:", (new Date() - startTime)/1000 , "seconds", "| Log Time:",new Date());}
+  response.end(" | v=0\n");
+  console.log("Running On:" ,host, "| Total Requests:", ++requests,"| App Uptime:", (new Date() - startTime)/1000 , "seconds", "| Log Time:",new Date());
+}
 
 var www = http.createServer(handleRequest);
-www.listen(8081,function () {
+www.listen(8080,function () {
     startTime = new Date();;
     host = process.env.HOSTNAME;
     console.log ("Started At:",startTime, "| Running On: " ,host, "\n" );
+});
